3ad35d9d02b6ec24a2c17e8d9a1f728508555518
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-08-18 23:37:07 +0200

    Added utils for thinks how admins

---
+++ src/AdminUtils.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* A simple class with utils for the admin.
+*/
+
+use PhangoApp\PhaRoutes\Routes;
+
+class AdminUtils {
+
+	static public function set_admin_link($text_admin, $parameters)
+	{
+	
+		return Routes::make_module_url(ADMIN_FOLDER, 'index', 'index', array($text_admin), $get=$parameters);
+
+	}
+
+}
+
+?>
