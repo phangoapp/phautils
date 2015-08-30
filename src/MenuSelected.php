90131eb9877864eac1a4e4f07bca6fb5638adff3
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-08-30 15:03:18 +0200

    Added simple class for create menus

---
+++ src/MenuSelected.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+*
+* @author  Antonio de la Rosa <webmaster@web-t-sys.com>
+* @file
+* @package ExtraUtils/Menus
+*
+*
+*/
+
+namespace PhangoApp\PhaUtils;
+
+use PhangoApp\PhaView\View;
+
+class MenuSelected {
+
+    static public function menu_selected($activation, $arr_op, $type=0)
+    {
+
+        echo View::load_view(array($activation, $arr_op, $type), 'common/utils/menuselected');
+
+    }
+    
+}
+
+?>
